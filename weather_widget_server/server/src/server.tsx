60d773d6148cc473be7bcc812fd5faaebff40f7a
Vinojan Iruthayam <14731156+vino-dk@users.noreply.github.com>
2023-08-23 10:07:27 +0200

    update server with template and root replacement

--- weather_widget_server/server/src/server.tsx
+++ weather_widget_server/server/src/server.tsx
@@ -8,15 +8,42 @@ import App from '../../client/src/App'
 const API_KEY: string = "166d00e26d3ff2c6149e89feccc5c59a";
 const app: Application = express();
 
-app.get("/", (req: Request, res: Response): void => {
-    const app = renderToString(<App />)
-    res.send(`<html>
-        <body>
-            <div id="root">${app}</div>
-        </body>
-    </html>`);
-    // res.send(app);
-});
+const pageTemplate = `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta
+      name="description"
+      content="Weather result"
+    />
+    <title>Weather App</title>
+    <script src="app.js" async defer></script>
+  </head>
+  <body>
+    <noscript>You need to enable JavaScript to run this app.</noscript>
+    <div id="root"></div>
+  </body>
+</html>
+`;
+
+const renderPage = (reactComponent) => {
+    const renderedComponent = renderToString(reactComponent);
+    return pageTemplate.replace('<div id="root"></div>', `<div id="root">${renderedComponent}</div>`);
+};
+
+// app.get("/", (req: Request, res: Response): void => {
+//     const app = renderToString(<App />)
+//     res.send(`<html>
+//         <body>
+//             <div id="root">${app}</div>
+//         </body>
+//     </html>`);
+//     // res.send("Helloooooo");
+// });
+
+app.get("/*", (req: Request, res: Response) => res.send(
+    renderPage(<App/>),
+));
 
 app.get('/weather', cors(), async (req: Request, res: Response): Promise<void> => {
     const city: string = (req.query.city as string) || 'Copenhagen';
