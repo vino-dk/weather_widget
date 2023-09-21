0c5c25575f940f1167f6c5d6748e364ffe583e3d
Vinojan Iruthayam <14731156+vino-dk@users.noreply.github.com>
2023-09-21 11:20:59 +0200

    Add Tailwind success, center widgets

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <h1>Weather in Denmark</h1>
-      <div className='App'>
+      <div className='App justify-center'>
         <WeatherWidget city="Copenhagen" />
         <WeatherWidget city="Odense" />
         <WeatherWidget city="Århus" />
